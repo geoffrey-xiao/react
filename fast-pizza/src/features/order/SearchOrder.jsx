0922075d00d9b317bff06b4976622f0650abc699
geoffrey <1377499035@qq.com>
2024-05-05 08:33:25 +0800

    init project && react-router loader action

---
+++ fast-pizza/src/features/order/SearchOrder.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`order/${query}`);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
